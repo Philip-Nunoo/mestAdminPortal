94089402e7f29c5a79df90721581d40708d43763
Philip A. Nunoo <ketay99@gmail.com>
2015-03-03 16:25:38 +0000

    work on the dashboard page

--- both/router.js
+++ both/router.js
@@ -10,8 +10,37 @@ Router.route('/', (function() {
   name: 'home'
 });
 
+// Dashboard
 Router.route('/dashboard', (function() {
   this.render('dashboard');
 }), {
   name: 'dashboard'
+});
+
+// Dashboard {allProspects}
+Router.route('/prospects', (function() {
+  this.render('prospects');
+}), {
+  name: 'allProspects'
+});
+
+// Dashboard {allApplicants}
+Router.route('/applicants', (function() {
+  this.render('applicants');
+}), {
+  name: 'allApplicants'
+});
+
+// Dashboard {allEits}
+Router.route('/all_eits', (function() {
+  this.render('allEits');
+}), {
+  name: 'allEits'
+});
+
+// Dashboard {allAlumni}
+Router.route('/alumni', (function() {
+  this.render('alumni');
+}), {
+  name: 'allAlumni'
 });
