e35b9ab42987fa717df0e7fb58c8b13a2d0c855b
Philip A. Nunoo <ketay99@gmail.com>
2015-03-04 22:55:39 +0000

    Added accounts ui

---
+++ both/collections/eits.js
@@ -0,0 +1,68 @@
+// eits.js
+Eit = function (doc) {
+	// Parent class object of Eit
+	_.extend(this, doc);
+};
+
+_.extend(Eit.prototype, {});
+
+Eits = new Mongo.Collection('eits', {
+	transform: function (doc) { return new Eit(doc);}
+});
+
+Eits.attachSchema(new SimpleSchema({
+	firstName: {
+		type: String,
+		label: "First Name",
+		max: 300
+	},
+	lastName: {
+		type: String,
+		label: "Last Name",
+		max: 300
+	},
+	dateOfBirth: {
+		type: String,
+		label: "Date of Birth",
+		max: 300
+	},
+	programmeOfStudy: {
+		type: String,
+		label: "Program of study",
+		max: 300
+	},
+	college: {
+		type: String,
+		label: "College or Institution",
+		autoform: {
+			options: function(){
+				return [
+				{value: 'ust', label: 'KNUST'},
+				{value: 'ucc', label: 'UCC'},
+				{value: 'legon', label: 'Legon'}
+				]
+			}
+		}
+	},
+	summary: {
+		type: String,
+		label: "Tell us about yourself",
+		optional: false,
+		max: 1000
+	},
+	createdAt: {
+		type: Date,
+		autoform: {
+			omit: true
+		},
+		autoValue: function() {
+			if (this.isInsert) {
+				return new Date;
+			} else if (this.isUpsert) {
+				return {$setOnInsert: new Date};
+			} else {
+				this.unset();
+			}
+		}
+	}
+}));
--- client/applicants/js/applicants.js
+++ client/applicants/js/applicants.js
@@ -11,13 +11,38 @@ Template.applicants.events({
 		Applicants.remove(this._id);
 		console.log("Applicant removed");
 	},
+	'click a.newApplicant': function(event) {
+		// add new Applicant
+		event.preventDefault();
+		$('#myModal').foundation('reveal', 'open', {
+			close_on_background_click: false
+		});
+	},
 	'click a.loadCSV': function (event) {
 		// loadCSV
 		event.preventDefault();
 
 	},
 	"change #files": function (e) {
+		e.preventDefault();
 		var files = e.target.files || e.dataTransfer.files;
+		
+		for (var i = 0, file; file = files[i]; i++) {
+			if (file.type.indexOf("text") == 0) {
+				var reader = new FileReader();
+				
+				reader.onloadend = function (e) {
+					var text = e.target.result;
+					var all = $.csv.toObjects(text);
+					console.log(all)
+					_.each(all, function (entry) {
+						Applicants.insert(entry);
+					});
+				}
+				reader.readAsText(file);
+			}
+		}
+		/*var files = e.target.files || e.dataTransfer.files;
 		for (var i = 0, file; file = files[i]; i++) {
 			if (file.type.indexOf("text") == 0) {
 				var reader = new FileReader();
@@ -32,6 +57,6 @@ Template.applicants.events({
 				}
 				reader.readAsText(file);
 			}
-		}
+		}*/
 	}
 });
